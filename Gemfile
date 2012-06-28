6ef92e92930d8398a7591274b4b184e8f3d2b40d
Daniel Kummer <daniel.kummer@namics.com>
2012-06-28 16:05:47 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'mongoid', '~> 3.0.0.rc'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'capistrano'
+  gem 'rspec-rails'
+  gem 'mongoid-rspec'
+end
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'guard-rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VagrantCook::Application.config.secret_token = 'bca3222bd88fe5ca2c81783aae6846dccc5719006852058fb3e33fa21915eeba8b0f6819ad76d4bf73bb9ab70609556345501e8b8c9fa5c9f7c5f5ae9e97c2f1'
