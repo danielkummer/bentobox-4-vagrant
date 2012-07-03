29cf8d00c24b203277742465bf0626f645135ed0
Daniel Kummer <daniel.kummer@namics.com>
2012-07-03 21:42:17 +0200

    add routing specs

--- spec/routing/bentoboxes_routing_spec.rb
+++ spec/routing/bentoboxes_routing_spec.rb
@@ -7,28 +7,28 @@ describe BentoboxesController do
       get("/bentoboxes").should route_to("bentoboxes#index")
     end
 
-    it "routes to #new" do
-      get("/bentoboxes/new").should route_to("bentoboxes#new")
-    end
-
     it "routes to #show" do
       get("/bentoboxes/1").should route_to("bentoboxes#show", :id => "1")
     end
 
+    it "routes to #new" do
+      get("/users/1/bentoboxes/new").should route_to("bentoboxes#new", :user_id => "1")
+    end
+
     it "routes to #edit" do
-      get("/bentoboxes/1/edit").should route_to("bentoboxes#edit", :id => "1")
+      get("/users/1/bentoboxes/1/edit").should route_to("bentoboxes#edit", :id => "1", :user_id => "1")
     end
 
     it "routes to #create" do
-      post("/bentoboxes").should route_to("bentoboxes#create")
+      post("/users/1/bentoboxes").should route_to("bentoboxes#create", :user_id => "1")
     end
 
     it "routes to #update" do
-      put("/bentoboxes/1").should route_to("bentoboxes#update", :id => "1")
+      put("/users/1/bentoboxes/1").should route_to("bentoboxes#update", :id => "1",  :user_id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/bentoboxes/1").should route_to("bentoboxes#destroy", :id => "1")
+      delete("/users/1/bentoboxes/1").should route_to("bentoboxes#destroy", :id => "1", :user_id => "1")
     end
 
   end
