c9297363f601de431e168227a7728dce05ea0fb4
Daniel <dkummer@Daniels-MacBook-Pro.local>
2012-07-04 23:05:27 +0200

    fix vagrantbox in bentobox validation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
         format.json { render json: @category, status: :created, location: @category }
       else
         format.html { render action: "new" }
@@ -33,7 +33,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to categories_url, notice: 'Category was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
