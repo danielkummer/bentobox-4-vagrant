521dc3a069281f4f0189763fc2c162d89550f490
Daniel Kummer <daniel.kummer@namics.com>
2012-07-19 18:39:46 +0200

    update specs, fix fabricators - finally!!!

--- spec/fabricators/bentobox_fabricator.rb
+++ spec/fabricators/bentobox_fabricator.rb
@@ -2,13 +2,13 @@ Fabricator(:bentobox) do
   name "Bentobox"
   description "Description"
   public false
-  user { Fabricate(:users) }
+  user { Fabricate(:user) }
   vagrantbox { Fabricate(:vagrantbox) }
 end
 
 Fabricator(:bentobox_with_ingredient, from: :bentobox) do
   after_create do |bentobox|
-    bentobox.ingredients << Fabricate(:full_ingredient, bentobox: bentobox)
+    bentobox.ingredients.push(Fabricate.build(:full_ingredient))
   end
   #ingredients(count:1) { |bentobox| Fabricate(:full_ingredient, bentobox: bentobox) }
 end
