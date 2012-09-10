d055ec9ea5cf9d28bf25ea1e06f8883352db230f
Daniel Kummer <daniel.kummer@namics.com>
2012-09-10 20:55:10 +0200

    add seed data to tests - app configuration data

---
+++ db/seeds/test.rb
@@ -0,0 +1,6 @@
+AppConfiguration.put('validation.pem', 'LOAD THE KEY FILE!')
+AppConfiguration.put('validation client name', 'chef-validator')
+AppConfiguration.put('chef client node name', 'bentobox')
+AppConfiguration.put('chef client key', 'config/production.pem')
+AppConfiguration.put('chef server api url', 'http://localhost:4000')
+AppConfiguration.put('chef server admin url', 'http://localhost:4040')
