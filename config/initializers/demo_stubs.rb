5bb93c69c8fdf124b66a44492664a4ec5dd04ecc
Daniel Kummer <daniel.kummer@namics.com>
2012-09-13 18:04:20 +0200

    demo stub fix

--- config/initializers/demo_stubs.rb
+++ config/initializers/demo_stubs.rb
@@ -1,5 +1,6 @@
 if ENV['DEMO']
-  require 'webmock/rspec'
+  require 'webmock'
+  include WebMock::API
 
   stub_request(:delete, "http://localhost:4000/clients/").
     to_return(status: 200, body: {}.to_json, headers: {'Content-Type' => 'application/json'})
@@ -8,6 +9,6 @@ if ENV['DEMO']
     to_return(status: 200, headers: {'Content-Type' => 'application/json'}, body: {name: "some_client", admin: false, private_key: "----this is a private key-----"}.to_json)
 
   stub_request(:get, "http://localhost:4000/cookbooks?num_versions=1").
-    to_return(status:200, body: {'apache2' => '1.1.12'}.to_json, headers: {'Content-Type' => 'application/json'})
+    to_return(status:200, body: {'apache2' => '1.1.12', 'mysql' => '5', 'demo entry' => '1.0'}.to_json, headers: {'Content-Type' => 'application/json'})
 end
 
